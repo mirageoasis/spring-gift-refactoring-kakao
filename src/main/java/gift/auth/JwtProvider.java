dcab54443ee05bfc5ab6673d9f1664c0d5388009
cheesecrust <jack0810@naver.com>
2026-02-26 15:52:42 +0900

    refactor: 미사용 @Autowired import 제거

--- src/main/java/gift/auth/JwtProvider.java
+++ src/main/java/gift/auth/JwtProvider.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 import javax.crypto.SecretKey;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
